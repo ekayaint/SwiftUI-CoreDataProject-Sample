0f9665ce070a2c136ed213aee45d3ce6532b3aa9
ekayaint <ekayaint@gmail.com>
2023-10-12 19:03:55 +0300

    Initial Commit

---
+++ SwiftUI-CoreDataProject-Sample/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftUI-CoreDataProject-Sample
+//
+//  Created by ekayaint on 12.10.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUI-CoreDataProject-Sample/SwiftUI_CoreDataProject_SampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_CoreDataProject_SampleApp.swift
+//  SwiftUI-CoreDataProject-Sample
+//
+//  Created by ekayaint on 12.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_CoreDataProject_SampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
