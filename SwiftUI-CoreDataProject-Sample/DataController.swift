ab6d66fd9d8127ac1e56a424c2a88a788e5dcf13
ekayaint <ekayaint@gmail.com>
2023-10-12 19:07:45 +0300

    Started

---
+++ SwiftUI-CoreDataProject-Sample/DataController.swift
@@ -0,0 +1,21 @@
+//
+//  DataController.swift
+//  SwiftUI-CoreDataProject-Sample
+//
+//  Created by ekayaint on 12.10.2023.
+//
+
+import Foundation
+import CoreData
+
+class DataController: ObservableObject {
+    let container = NSPersistentContainer(name: "Bookworm")
+    
+    init() {
+        container.loadPersistentStores { description, error in
+            if let error = error {
+                print("Core Data failed to load: \(error.localizedDescription)")
+            }
+        }
+    }
+}
--- SwiftUI-CoreDataProject-Sample/SwiftUI_CoreDataProject_SampleApp.swift
+++ SwiftUI-CoreDataProject-Sample/SwiftUI_CoreDataProject_SampleApp.swift
@@ -9,9 +9,12 @@ import SwiftUI
 
 @main
 struct SwiftUI_CoreDataProject_SampleApp: App {
+    @StateObject private var dataController = DataController()
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .environment(\.managedObjectContext, dataController.container.viewContext)
         }
     }
 }
